0fcee399b6eab71e31fcf61de2c54ead3b6621a1
maipham12 <s3978365@rmit.edu.vn>
2024-04-02 08:54:05 +0700

    finalise

--- src/models/Dependent.java
+++
@@ -1,9 +0,0 @@
-package models;
-public class Dependent extends Customer {
-    public Dependent(String id, String fullName) {
-        super(id, fullName);
-    }
-
-    // Getters and Setters
-
-}
